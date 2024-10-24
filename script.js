e1dcbbe8072a4f7b3b4e9543e57dc7602cf7a5ba
Rocky Wang <rocky.wang2@student.tdsb.on.ca>
2024-10-24 11:23:15 -0400

    fixed script.ja code error

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ function HeronsFormula (a, b, c){
     let a2 = a * a;
     let b2 = b * b;
     let c2 = c * c;
-    return {
-        Math.sqrt((4*a2*b2) - ((a2 + b2 - c2)*(a2 + b2 - c2)))/4
-    }
+
+    return (Math.sqrt((4*a2*b2) - ((a2 + b2 - c2)*(a2 + b2 - c2))))/4;
 }
 
 function AmbiguousCase (angleA, sideA, sideB){
@@ -23,13 +22,13 @@ function AmbiguousCase (angleA, sideA, sideB){
     
         }else if (sideA > sideB){
     
-        }else if (h < sideA) && (a < b){
+        }else if ((h < sideA) && (a < b)){
     
         }else{
-    
+            
         }
     } else if (angleA > 90){
-        if (a < b) || (a == h){
+        if ((a < b) || (a == h)){
 
         }else if (a > b){
 
